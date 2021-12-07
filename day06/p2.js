dc3452e54fc19e97a3d2490eee2fd0aa3cc32f6e
Ryan Tenorio <ryantenorio@gmail.com>
2021-12-06 21:33:28 -0800

    day6 done

--- day06/p2.js
+++ day06/p2.js
@@ -32,18 +32,48 @@ class Lanternfish {
 
 // create initial state
 let currentDay = 0;
-let currentState = data.map(startingDaysUntilBreed => {
-  return new Lanternfish(startingDaysUntilBreed);
-});
-const targetDay = 3;
+const targetDay = 256;
+let school = {
+  0: 0,
+  1: 0,
+  2: 0,
+  3: 0,
+  4: 0,
+  5: 0,
+  6: 0,
+  7: 0,
+  8: 0,
+}
+data.forEach(startingDay => {
+  school[startingDay] = school[startingDay] + 1;
+})
 while(currentDay < targetDay) {
-  let newFish = [];
-  currentState.forEach(fish => {
-    if(fish.incrementAndCheckShouldSpawn()) {
-      newFish.push(new Lanternfish(8));
-    }
-  });
-  currentState = currentState.concat(newFish);
+  let newFish = school[0];
+  school[0] = school[1];
+  school[1] = school[2];
+  school[2] = school[3];
+  school[3] = school[4];
+  school[4] = school[5];
+  school[5] = school[6];
+  school[6] = school[7] + newFish;
+  school[7] = school[8];
+  school[8] = newFish;
   currentDay++;
 }
-console.log(currentState.length);
+console.log(school);
+console.log(school[0] + school[1] + school[2] + school[3] + school[4] + school[5] + school[6] + school[7] + school[8]);
+// let currentState = data.map(startingDaysUntilBreed => {
+//   return new Lanternfish(startingDaysUntilBreed);
+// });
+// const targetDay = 256;
+// while(currentDay < targetDay) {
+//   let newFish = [];
+//   currentState.forEach(fish => {
+//     if(fish.incrementAndCheckShouldSpawn()) {
+//       newFish.push(new Lanternfish(8));
+//     }
+//   });
+//   currentState = currentState.concat(newFish);
+//   currentDay++;
+// }
+// console.log(currentState.length);
